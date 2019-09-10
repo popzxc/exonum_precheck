b4b36a778a08020f5ab56a2239c2af94bdffeea1
Igor Aleksanov <popzxc@yandex.ru>
2019-09-10 16:03:47 +0300

    Add license and readme

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
-from distutils.core import setup
+import setuptools
 
 PYTHON_REQUIRES = ">=3.4"
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="exonum_precheck",
     version="0.1",
+    author="Igor Aleksanov",
     description="Exonum precheck deployment script",
+    url="https://github.com/popzxc/exonum_precheck",
     packages=["exonum_precheck"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[],
     python_requires=PYTHON_REQUIRES,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Security :: Cryptography",
+    ],
 )
