1966d34176a786e515a3ca40ac41edbe563b1f39
Igor Aleksanov <popzxc@yandex.ru>
2019-09-10 16:08:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@
 import setuptools
 
 PYTHON_REQUIRES = ">=3.4"
+INSTALL_REQUIRES = ["pyyaml"]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="exonum_precheck",
-    version="0.1",
+    version="0.1.1",
     author="Igor Aleksanov",
     description="Exonum precheck deployment script",
     url="https://github.com/popzxc/exonum_precheck",
     packages=["exonum_precheck"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=[],
+    install_requires=INSTALL_REQUIRES,
     python_requires=PYTHON_REQUIRES,
     classifiers=[
         "Programming Language :: Python :: 3",
